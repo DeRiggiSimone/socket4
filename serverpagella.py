3adea7c702095462e3d9679b4400e02ab34ca1ac
DeRiggiSimone <117725950+DeRiggiSimone@users.noreply.github.com>
2024-02-02 11:11:15 +0100

    Update serverpagella.py

--- serverpagella.py
+++ serverpagella.py
@@ -5,6 +5,21 @@ SERVER_IP="127.0.0.1"
 SERVER_PORT=22005
 BUFFER_SIZE=1024
 
+dizionarioStudenti={   'Antonio Barbera': [   ['Matematica', 8, 1],
+                            ['Italiano', 6, 1],
+                            ['Inglese', 9.5, 0],
+                            ['Storia', 8, 2],
+                            ['Geografia', 8, 1]],
+        'Giuseppe Gullo': [   ['Matematica', 9, 0],
+                           ['Italiano', 7, 3],
+                           ['Inglese', 7.5, 4],
+                           ['Storia', 7.5, 4],
+                           ['Geografia', 5, 7]],
+        'Nicola Spina': [   ['Matematica', 7.5, 2],
+                         ['Italiano', 6, 2],
+                         ['Inglese', 4, 3],
+                         ['Storia', 8.5, 2],
+                         ['Geografia', 8, 2]]}
 
 with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as sock_server:
     sock_server.bind((SERVER_IP,SERVER_PORT))
@@ -20,6 +35,10 @@ with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as sock_server:
                     break
                 data=data.decode()
                 data=json.loads(data)
+                comando=data['comando']
+                parametro=data['parametro']
+                print(comando)
+                print(parametro)
     
                 ris=str(ris)
                 print("risul:",ris)
